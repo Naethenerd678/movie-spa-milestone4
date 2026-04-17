3ae977661468daa4f0edbebf54ca3ff4c536a1fb
Naethenerd678 <161889617+Naethenerd678@users.noreply.github.com>
2026-04-16 21:16:55 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,76 @@
+let currentPage = 1;
+let currentQuery = "";
+
+const API_KEY = "your_actual_key_here";
+
+// SEARCH BUTTON
+$("#searchBtn").click(function () {
+  currentQuery = $("#searchInput").val();
+  currentPage = 1;
+  searchMovies();
+});
+
+function searchMovies() {
+  $.ajax({
+    url: "https://api.themoviedb.org/3/search/movie",
+    method: "GET",
+    data: {
+      api_key: API_KEY,
+      query: currentQuery,
+      page: currentPage
+    },
+    success: function (data) {
+      renderMovies(data.results);
+      $("#pageInfo").text(`Page ${data.page} of ${data.total_pages}`);
+    }
+  });
+}
+
+// RENDER MOVIES
+function renderMovies(movies) {
+  const template = $("#movie-template").html();
+  let html = "";
+
+  movies.forEach(movie => {
+    html += Mustache.render(template, movie);
+  });
+
+  $("#results").html(html);
+}
+
+// CLICK MOVIE → DETAILS
+$(document).on("click", ".movie-card", function () {
+  const id = $(this).data("id");
+
+  $.ajax({
+    url: `https://api.themoviedb.org/3/movie/${id}`,
+    data: { api_key: API_KEY },
+    success: function (movie) {
+      const template = $("#details-template").html();
+      const html = Mustache.render(template, movie);
+      $("#details").html(html);
+    }
+  });
+});
+
+// PAGINATION
+$("#nextBtn").click(function () {
+  currentPage++;
+  searchMovies();
+});
+
+$("#prevBtn").click(function () {
+  if (currentPage > 1) {
+    currentPage--;
+    searchMovies();
+  }
+});
+
+// VIEW SWITCH
+$("#gridBtn").click(() => {
+  $("#results").removeClass("list-view").addClass("grid-view");
+});
+
+$("#listBtn").click(() => {
+  $("#results").removeClass("grid-view").addClass("list-view");
+});
